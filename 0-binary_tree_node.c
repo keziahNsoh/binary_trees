4f1c3467f3eda47226c04858a2f2222515c81ae2
keziahNsoh <porepeya@gmail.com>
2024-06-04 19:35:30 +0000

    New node

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -3,7 +3,7 @@
 
 /**
  * binary_tree_node - Creates a new binary tree node
- * @parent: Pointer to the parent node
+ * @parent: Tis is the pointer to the parent node
  * @value: Value to put in the new node
  * Return: Pointer to the new node, or NULL on failure
  */
