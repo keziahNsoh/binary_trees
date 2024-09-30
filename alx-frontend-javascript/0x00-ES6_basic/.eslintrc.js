0e964e3d2711d191e4d026c09979978227bb0719
keziahNsoh <porepeya@gmail.com>
2024-09-30 16:33:07 +0000

    correction

---
+++ alx-frontend-javascript/0x00-ES6_basic/.eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+    jest: true,
+  },
+  extends: 'eslint:recommended',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  rules: {
+    // Add any specific linting rules you need
+  },
+};
+
